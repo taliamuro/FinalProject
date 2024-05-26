b43877b7a03c6bf4aea7a0b097c1f4208f33f8fd
taliamuro <taliamuro3@gmail.com>
2024-05-26 11:17:57 -0400

    Adding documentation to the methods in Address class

--- src/main/java/org/talia/Address.java
+++ src/main/java/org/talia/Address.java
@@ -35,15 +35,11 @@ public class Address {
         }
     }
 
-    public static boolean isPostalCodeValid(String postalCode) {
-        if (postalCode.length() == 6) {
-            return isValid6(postalCode);
-        } else if (postalCode.length() == 7) {
-            return isValid7(postalCode);
-        }
-        return false;
-    }
-
+    /**
+     * Formats a postal code to be CDCDCD if it has a length of 6
+     * @param postalCode the input string
+     * @return the postal code in CDCDCD format
+     */
     public static boolean isValid6(String postalCode) {
         return Character.isLetter(postalCode.charAt(0)) &&
                 Character.isDigit(postalCode.charAt(1)) &&
@@ -53,6 +49,11 @@ public class Address {
                 Character.isDigit(postalCode.charAt(5));
     }
 
+    /**
+     * Formats a postal code to be CDC DCD if it has a length of 7
+     * @param postalCode the input string
+     * @return the postal code in CDC DCD format
+     */
     public static boolean isValid7(String postalCode) {
         return Character.isLetter(postalCode.charAt(0)) &&
                 Character.isDigit(postalCode.charAt(1)) &&
@@ -61,5 +62,19 @@ public class Address {
                 Character.isDigit(postalCode.charAt(4)) &&
                 Character.isLetter(postalCode.charAt(5)) &&
                 Character.isDigit(postalCode.charAt(6));
+    }
+
+    /**
+     * Checks if a postal code is valid or not and formats it according to its length
+     * @param postalCode the input string
+     * @return the postal code in the correct format
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode.length() == 6) {
+            return isValid6(postalCode);
+        } else if (postalCode.length() == 7) {
+            return isValid7(postalCode);
+        }
+        return false;
     }
 }
